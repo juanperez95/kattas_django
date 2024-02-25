e4de601dd56c1985ac71ed92d21985663b5f4931
juanperez95 <jpperez2282@misena.edu.co>
2024-02-24 20:55:25 -0500

    Se ajusto dashboard y herencia de plantilla, enlaces iniciales

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("login/", loginView),
-    path("inicio/", indexView),
-    path("registro/", registroView)
+    path("login/", loginView,name="login"),
+    path("", indexView,name="inicio"),
+    path("registro/", registroView,name="registro_usuarios"),
+    path("dashboard/", dashboard_base,name="dashboard_base"),
+    path("insumo_admin/", dashboard_insumos,name="dashboard_insumo"),
+    path("users_admin/", dashboard_usuarios,name="dashboard_usuarios"),
+    path("productos_admin/", dashboard_productos,name="dashboard_productos"),
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -5,6 +5,19 @@ def loginView(request):
 
 def indexView(request):
     return render(request, "cliente/index.html")
+
 def registroView(request):
     return render(request, "cliente/registro.html")
+
+def dashboard_base(request):
+    return render(request,"cliente/dashboard.html")
+
+def dashboard_insumos(request):
+    return render(request,"cliente/dashboard_insumo.html")
+
+def dashboard_usuarios(request):
+    return render(request,"cliente/dashboard_usuarios.html")
+
+def dashboard_productos(request):
+    return render(request,"cliente/dashboard_productos.html")
     
